0b0c8e6f2f412aad4bc22499ff0d5b7b5d2b3063
Sbipul <bipulcroy121@gmail.com>
2022-11-10 11:04:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const readline = require("readline");
+try {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("", function (answer) {
+    const num = parseInt(answer);
+    let str = [];
+    if (1 <= num && num <= 100) {
+      for (let index = 0; index < num; index++) {
+        if (index % 2 !== 0) {
+          str.push("that I love it ");
+        } else {
+          str.push("I hate it ");
+        }
+      }
+    }
+    let newStr = "";
+    str.forEach((strin) => {
+      newStr = `${newStr} ${strin}`;
+    });
+    console.log(newStr);
+  });
+} catch (error) {}
